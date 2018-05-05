39cc1aa3762a86b8d6d6e54f6731dc3c3d8dce86
Nikita Rokotyan <nrokotyan@gmail.com>
2018-05-05 11:53:30 +0200

    Legend: moving to a separate component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Map from './map/map.js'
+import Legend from './legend'
+
+// Styles
+import './App.css'
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="vis-map">
         <Map/>
+        <Legend />
       </div>
     );
   }
